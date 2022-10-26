80407f9b98e39f167affcac50ce7a0948f521a5b
Dravrmipad <116707722+Dravrmipad@users.noreply.github.com>
2022-10-26 17:33:33 +0530

    Add files via upload

---
+++ .github/workflows/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+    using namespace std;
+int main(){
+
+    int x, y ;
+ cout<<"x:";
+    cin>>x;
+cout<<"y:";
+    cin>>y;
+
+    int z;
+
+    z=y;
+    y=x;
+    x=z;
+
+    cout<<"x:"<<x<<endl;
+    cout<<"y:"<<y<<endl;
+    
+
+    return 0;
+}
